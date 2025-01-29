457f097fffc10576bd54c61b7e2c2495e6b40cc5
Orest Kozak <orestkozak17@gmail.com>
2025-01-29 13:06:32 +0200

    added migrations for railway db

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/exception.filter';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
